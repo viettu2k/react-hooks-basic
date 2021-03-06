11669380546526739c95c66f37859c823267a6e6
unknown <doanviettu2k@gmail.com>
2021-03-06 09:08:15 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+//import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+    ]);
+    function handleToDoClick(todo) {
+      console.log(todo);
+      const index = todoList.findIndex( x => x.id === todo.id);
+      if (index < 0) return;
+
+      const newTodoList = [...todoList]
+      newTodoList.splice(index, 1);
+      setTodoList(newTodoList);
+    }
   return (
     <div className="app">
-      <h1> welcome to react hooks</h1>
-      <ColorBox/>
+      <h1>React hooks - TodoList</h1>
+      <TodoList todos={todoList} onTodoClick={handleToDoClick}/>
     </div>
   );
 }
